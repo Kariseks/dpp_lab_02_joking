cff0fb01bde944ac8f1fa7428f68e022eea622ba
Karol Gasior <gasiorkarolpl@gmail.com>
2026-03-18 09:48:49 +0100

    feat:Add adding comments in CommentDAO

--- dpp_2/gui/src/main/java/pl/pwr/model/DAO/CommentDAO.java
+++ dpp_2/gui/src/main/java/pl/pwr/model/DAO/CommentDAO.java
@@ -1,4 +1,28 @@
 package pl.pwr.model.DAO;
 
+import pl.pwr.model.DBConnector;
+import pl.pwr.model.entities.Comment;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
 public class CommentDAO {
+
+    public void addComment(Comment comment) {
+        String sql = "INSERT INTO comments (text, rating, joke_id, user_id) VALUES (?, ?, ?, ?)";
+
+        try (Connection conn = DBConnector.getConnection();
+             PreparedStatement pstmt = conn.prepareStatement(sql)) {
+
+            pstmt.setString(1, comment.text());
+            pstmt.setInt(2, comment.rating());
+            pstmt.setInt(3, comment.jokeId());
+            pstmt.setInt(4, comment.userId());
+
+            pstmt.executeUpdate();
+        } catch (SQLException e) {
+            throw new RuntimeException("Błąd podczas dodawania komentarza do bazy danych: " + e.getMessage(), e);
+        }
+    }
 }
